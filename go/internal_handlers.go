4be2a5477e528a5021b82ccd7691fc7b16170298
wass88 <wasss80@gmail.com>
2024-12-08 16:58:27 +0900

    Delete last

--- go/internal_handlers.go
+++ go/internal_handlers.go
@@ -96,31 +96,5 @@ func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// 50%の確率でキューの最初から取り出す
-	if rand.Intn(100) < 0 {
-		w.WriteHeader(http.StatusNoContent)
-		return
-	}
-
-	ride = &RideType{}
-	if err := db.GetContext(ctx, ride, `SELECT id FROM rides WHERE chair_id IS NULL ORDER BY created_at DESC LIMIT 1`); err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			w.WriteHeader(http.StatusNoContent)
-			return
-		}
-		writeError(w, http.StatusInternalServerError, err)
-		return
-	}
-
-	missing, err = matcher(ctx, db, ride)
-	if err != nil {
-		writeError(w, http.StatusInternalServerError, err)
-		return
-	}
-	if missing {
-		w.WriteHeader(http.StatusNoContent)
-		return
-	}
-
 	w.WriteHeader(http.StatusNoContent)
 }
