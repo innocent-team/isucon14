33dd2f6bdeebb2f25704ad166a7fdb834dadc561
nonylene <nonylene@gmail.com>
2024-12-08 16:33:51 +0900

    filter

--- go/owner_handlers.go
+++ go/owner_handlers.go
@@ -233,13 +233,16 @@ FROM chairs
                           SUM(IFNULL(distance, 0)) AS total_distance,
                           MAX(created_at)          AS total_distance_updated_at
                    FROM (SELECT chair_id,
-                                created_at,
+                                chair_locations.created_at,
                                 ABS(latitude - LAG(latitude) OVER (PARTITION BY chair_id ORDER BY created_at)) +
                                 ABS(longitude - LAG(longitude) OVER (PARTITION BY chair_id ORDER BY created_at)) AS distance
+    INNER JOIN chairs
+    ON chair_locations.chair_id = chairs.id
+    WHERE owner_id = ?
                          FROM chair_locations) tmp
                    GROUP BY chair_id) distance_table ON distance_table.chair_id = chairs.id
 WHERE owner_id = ?
-`, owner.ID); err != nil {
+`, owner.ID, owner.ID); err != nil {
 		ownerGetChairsCacheMutex.Unlock()
 		writeError(w, http.StatusInternalServerError, err)
 		return
