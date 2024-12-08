f1803bc61594f9167b39b3342ebd0320ccdb4e9b
wass80 <wasss80@gmail.com>
2024-12-08 12:44:20 +0900

    4-copy-latest-chair-locations.sql

--- sql/1-schema.sql
+++ sql/1-schema.sql
@@ -51,6 +51,16 @@ CREATE TABLE chair_locations
 )
   COMMENT = '椅子の現在位置情報テーブル';
 
+DROP TABLE IF EXISTS latest_chair_locations;
+CREATE TABLE latest_chair_locations
+(
+  chair_id   VARCHAR(26) NOT NULL COMMENT '椅子ID',
+  latitude   INTEGER     NOT NULL COMMENT '経度',
+  longitude  INTEGER     NOT NULL COMMENT '緯度',
+  PRIMARY KEY (chair_id)
+)
+  COMMENT = '椅子の最新位置情報テーブル';
+
 DROP TABLE IF EXISTS users;
 CREATE TABLE users
 (
