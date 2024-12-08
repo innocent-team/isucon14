d9cfb26fd269d7433e81b8bb6a2e0623c4dbeb06
triple-innocent <isucon-triple-innocent@googlegroups.com>
2024-12-08 03:33:45 +0000

    deploy.sh

---
+++ conf/env/2/env.sh
@@ -0,0 +1,11 @@
+ISUCON_DB_HOST="127.0.0.1"
+ISUCON_DB_PORT="3306"
+ISUCON_DB_USER="isucon"
+ISUCON_DB_PASSWORD="isucon"
+ISUCON_DB_NAME="isuride"
+
+# マッチング間隔（秒）
+ISUCON_MATCHING_INTERVAL=0.5
+
+GOOGLE_APPLICATION_CREDENTIALS=/home/isucon/service-account-credentials.json
+GOOGLE_CLOUD_PROJECT=triple-innocent
---
+++ deploy.sh
@@ -0,0 +1,78 @@
+#! /bin/bash
+
+cd `dirname $0`
+
+echo "Current Host: $HOSTNAME"
+
+if [[ "$HOSTNAME" == isucon1 ]]; then
+  INSTANCE_NUM="1"
+elif [[ "$HOSTNAME" == isucon2 ]]; then
+  INSTANCE_NUM="2"
+elif [[ "$HOSTNAME" == isucon3 ]]; then
+  INSTANCE_NUM="3"
+else
+  echo "Invalid host"
+  exit 1
+fi
+
+set -ex
+
+git pull
+
+sudo systemctl daemon-reload
+
+# env
+install -o isucon -g isucon -m 755 ./conf/env/${INSTANCE_NUM}/env.sh /home/isucon/env.sh
+
+# NGINX
+sudo install -o root -g root -m 644 ./conf/nginx/conf.d/isuride.conf /etc/nginx/conf.d/isuride.conf
+sudo install -o root -g root -m 644 ./conf/nginx/nginx.conf /etc/nginx/nginx.conf
+
+if [[ "$INSTANCE_NUM" == 1 ]]; then
+  sudo nginx -t
+
+  sudo systemctl restart nginx
+  sudo systemctl enable nginx
+else
+  sudo systemctl disable --now nginx.service
+fi
+
+# otel-contrib
+# credential があるので config ファイルは含めていない
+sudo systemctl enable --now otelcol-contrib.service
+
+# APP
+sudo install -o root -g root -m 644 ./conf/systemd/system/isuride-go.service /etc/systemd/system/isuride-go.service
+
+if [[ "$INSTANCE_NUM" == 1 ||  "$INSTANCE_NUM" == 2 ]]; then
+  sudo systemctl daemon-reload
+
+  pushd go
+  go build -o isuride
+  popd
+  sudo systemctl restart isuride-go.service
+  sudo systemctl enable --now isuride-go.service
+
+  sleep 2
+
+  sudo systemctl status isuride-go.service --no-pager
+else
+  sudo systemctl disable --now isuride-go.service
+fi
+
+# MYSQL
+sudo install -o root -g root -m 644 ./conf/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo install -o root -g root -m 644 ./conf/systemd/system/mysql-digest.service /etc/systemd/system/mysql-digest.service
+
+if [[ "$INSTANCE_NUM" == 3 || "$INSTANCE_NUM" == 1 ]]; then
+  sudo systemctl daemon-reload
+
+  echo "MySQL restart したいなら手動で sudo systemctl restart mysql やってね"
+#  sudo systemctl restart mysql
+  sudo systemctl enable --now mysql
+  sudo systemctl enable --now mysql-digest
+  sudo systemctl restart --now mysql-digest
+else
+  sudo systemctl disable --now mysql.service
+  sudo systemctl disable --now mysql-digest.service
+fi
