917f16186179270a0396c75c6bd5eb1dfb1c0882
utgwkk <utagawakiki@gmail.com>
2024-12-08 10:32:47 +0900

    appGetNotificationに効くインデックス

--- sql/1-schema.sql
+++ sql/1-schema.sql
@@ -91,6 +91,7 @@ CREATE TABLE rides
   created_at            DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '要求日時',
   updated_at            DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '状態更新日時',
   PRIMARY KEY (id),
+  KEY user_id_created_at (user_id, created_at),
   KEY chair_id (chair_id)
 )
   COMMENT = 'ライド情報テーブル';
